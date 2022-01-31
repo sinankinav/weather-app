56e7d8922a079cec5d62f4b7ca31934e2d97dba9
sinankinav <78664500+sinankinav@users.noreply.github.com>
2022-02-01 01:27:47 +0300

    Update App.js
    math methodu eklendi

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ const search = evt =>{
 </div>
 <div className="weather-box">
    
- <div className="temp">{weather.main.temp}°c
+ <div className="temp">{Math.round(weather.main.temp)}°c
  </div>
  <div className="weather"> {weather.weather[0].main} </div>
 </div></div>
